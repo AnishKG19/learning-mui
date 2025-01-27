38a1f4f5c14324217844ce9a112eaabfcceee0e0
AnishKG19 <anishkumar.gupta.mat21@itbhu.ac.in>
2025-01-28 02:18:21 +0530

    added some stuff

--- components2/Layout/Header.jsx
+++ components2/Layout/Header.jsx
@@ -7,6 +7,8 @@ import {Link} from "react-router-dom"
 import MenuIcon from '@mui/icons-material/Menu';
 
 
+//  hello world!
+
 const Header = () => {
 
   const [mobileOpen , setMobileOpen] =useState(false)
